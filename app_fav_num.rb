4c55c79ec97ed07fef64feb375dab6895eb324f7
Chloe Krainbucher <chloekrainbucher@Chloes-MacBook-Pro.local>
2017-02-17 16:13:57 -0500

    fav nums sum

--- app_fav_num.rb
+++ app_fav_num.rb
@@ -18,8 +18,9 @@ end
 post '/favorite_numbers' do
 	backend_name = params[:name_input]
 	backend_age = params[:age_input]
-	backend_num1 = params[:fav_num1_input]
-	backend_num2 = params[:fav_num2_input]
-	backend_num3 = params[:fav_num3_input]
-	erb :total, :locals => {:username => backend_name, :age => backend_age, :fav1 => backend_num1, :fav2 => backend_num2, :fav3 => backend_num3}
+	backend_num1 = params[:fav_num1_input].to_i
+	backend_num2 = params[:fav_num2_input].to_i
+	backend_num3 = params[:fav_num3_input].to_i
+	sum = (backend_num1 + backend_num2 + backend_num3)
+	erb :total, :locals => {:username => backend_name, :age => backend_age, :fav1 => backend_num1, :fav2 => backend_num2, :fav3 => backend_num3, :sum => sum}
 end
