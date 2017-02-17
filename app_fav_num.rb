c8c2b84f1c4f68ee0ad8139c4062d1cfdf55db5e
Chloe Krainbucher <chloekrainbucher@Chloes-MacBook-Pro.local>
2017-02-17 16:23:25 -0500

    All done:)

--- app_fav_num.rb
+++ app_fav_num.rb
@@ -17,10 +17,18 @@ end
 
 post '/favorite_numbers' do
 	backend_name = params[:name_input]
-	backend_age = params[:age_input]
+	backend_age = params[:age_input].to_i
 	backend_num1 = params[:fav_num1_input].to_i
 	backend_num2 = params[:fav_num2_input].to_i
 	backend_num3 = params[:fav_num3_input].to_i
 	sum = (backend_num1 + backend_num2 + backend_num3)
-	erb :total, :locals => {:username => backend_name, :age => backend_age, :fav1 => backend_num1, :fav2 => backend_num2, :fav3 => backend_num3, :sum => sum}
+
+	results = 'is less'
+		if (sum > backend_age)
+			results = 'is greater'
+		else
+			results
+		end
+
+	erb :total, :locals => {:username => backend_name, :age => backend_age, :fav1 => backend_num1, :fav2 => backend_num2, :fav3 => backend_num3, :sum => sum, :results => results}
 end
